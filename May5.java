b45a4412e873c4fc7905c297b50d2dff13d4e951
Janaki <janakinanu26@gmail.com>
2026-05-29 20:16:04 +0530

    Add files via upload

---
+++ May5.java
@@ -0,0 +1,122 @@
+import java.util.Scanner;
+
+import static java.lang.Math.floor;
+import static java.lang.Math.round;
+
+public class May5 {
+    public static void main( String[] args){
+        Scanner n= new Scanner(System.in);
+
+        // Even/Odd
+//        System.out.print("Enter the number :");
+//        int a=n.nextInt();
+//        if(a%2==0){
+//            System.out.print("Even");
+//        }
+//        else{
+//            System.out.print("Odd");
+//        }
+
+        //Prime Number
+
+//        System.out.print("Enter number:");
+//        int b= n.nextInt();
+//        boolean isPrime=true;
+//        for(int i=2; i<b; i++){
+//            if(b%i==0) {
+//                isPrime = false;
+//            }
+//        }
+//        if(isPrime==true){
+//            System.out.print("Prime");
+//        }
+//        else {
+//            System.out.print("Not Prime");
+//        }
+
+        //Palindrome
+
+//        System.out.print("Enter numbers: ");
+//        int c=n.nextInt();
+//        int sum=0;
+//        int original=c;
+//        while (c!=0){                            // 678%10=78  sum=78 c=67
+//            int r=c%10;
+//            sum=sum*10+r;
+//            c=round(c/10);
+//        }
+//        if(sum==original){
+//            System.out.print("Palindrome");
+//        }
+//        else{
+//            System.out.print("Not palindrome");
+//        }
+
+        //Amstrong Number
+//        System.out.print("Enter Numbers: ");
+//        int d= n.nextInt();
+//        int original=d;
+//        int sum=0;
+//        while(d!=0){
+//            int r=d%10;// r=123%10= 3 12%10=2
+//            sum=sum+(r*r*r); //sum=3*3*3=81  81+8=90 91+1=91
+//            d=round(d/10); // d=123/10=12
+//
+//        }
+//        if(sum==original){
+//            System.out.print("Amstrong");
+//        }
+//        else{
+//            System.out.print("Not Amstrong");
+//        }
+
+        //Factorial
+
+//        System.out.print("Enter the number:");
+//        int e= n.nextInt();
+//        int fact=1;
+//        for(int i=1; i<e; i++){
+//            fact=fact*i;  // fact=1*1=1 1*2 2*3 3*4 4*5 5*6 6*7
+//        }
+//        System.out.print(fact);
+
+        //Fibonacci
+
+//        System.out.print("Enter the number of series:");    // 0,1,1,2,3,5
+//        int f= n.nextInt();
+//        int a= 0;
+//        int b=1;
+//        System.out.print(a+","+b+",");
+//        for (int i=1;i<f; i++){
+//            int c=a+b; // c=0+1=1  c=1+1=2
+//            System.out.print(c+",");
+//            a=b;//a=1 1
+//            b=c;//b=1 2
+//        }
+
+
+        //Reverse a number
+//        System.out.print("Enter the number:");
+//        int a=n.nextInt();
+//        int sum=0;
+//        while(a!=0){
+//            int r=a%10;
+//            sum=sum*10+r;
+//            a=round(a/10);
+//        }
+//        System.out.print(sum);
+
+        //Sum of digits
+
+//        System.out.print("Enter the number:");
+//        int a=n.nextInt();
+//        int sum=0;
+//        while(a!=0){
+//            int r=a%10;
+//            sum=sum+r;
+//            a=round(a/10);
+//        }
+//        System.out.print(sum);
+
+    }
+}
